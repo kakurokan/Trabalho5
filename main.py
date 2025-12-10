d1d781936dee4cae2ac3e2b844e830475ed353fa
kakurokan <leokakali7@gmail.com>
2025-12-10 18:06:04 +0000

    leitura de dados finalizada

--- main.py
+++ main.py
@@ -1,4 +1,117 @@
+from setuptools.package_index import local_open
+from sympy import (
+    Matrix,
+    sympify,
+    SympifyError,
+    symbols,
+    E,
+    pi,
+)
+from sympy.parsing.sympy_parser import (
+    standard_transformations,
+    implicit_multiplication_application,
+    convert_xor,
+    parse_expr,
+)
+
+
+def ler_valor_matematico(entrada):
+    # Configuração para entender 'pi', 'e', '^', multiplicação implícita
+    transformations = standard_transformations + (
+        implicit_multiplication_application,
+        convert_xor,
+    )
+    local_context = {"pi": pi, "e": E}
+
+    expr = parse_expr(
+        entrada, local_dict=local_context, transformations=transformations
+    )
+
+    return expr
+
+
+def criar_matriz(elementos):
+    matrix = [sympify(n) for n in elementos]
+    return Matrix(matrix)
+
+
+def norma(matriz_a, matriz_b):
+    resultado = matriz_b - matriz_a
+    return abs(max(resultado, key=abs))
+
+
+def variables(n):
+    locals_vars = {
+        "e": E,
+        "pi": pi,
+    }
+    x = "x"
+    for i in range(1, n + 1):
+        var = x + str(i)
+        locals_vars[var] = symbols(var, real=True)
+
+    return locals_vars
+
+
+def ler_funcao(fi, vars):
+    transformations = standard_transformations + (
+        implicit_multiplication_application,
+        convert_xor,
+    )  # Permite que o parse aceite '^' = **, 'ax' = a * x, e^a = exp(a)
+    fi = parse_expr(
+        fi, local_dict=vars, transformations=transformations
+    )  # Converte o input numa expressão sympy
+    return fi
+
+
 def main():
-    print()
+    print("*" * 65)
+    print(
+        "   Bem-vindo ao Solucionador de sistemas não lineares lineares utilizando o método de Newton"
+    )
+    print("*" * 65)
+
+    rodando = True
+
+    while rodando:
+        try:
+            n = int(input("\nInsira o número de equações/incógnitas: "))
+            if n <= 0:
+                raise ValueError("Número inválido")
+
+            local_vars = variables(n)
+
+            print(f"Insira a {n} funções (f(x) = 0)")
+            funcoes = []
+            for i in range(n):
+                fi_str = input(f"Insira a função f_{i + 1}(x): ").strip()
+                fi = ler_funcao(fi_str, local_vars)
+                funcoes.append(fi)
+            matriz_fi = criar_matriz(funcoes)
+
+            print(
+                f"Insira a aproximação inicial",
+                f"\nDigite os {n} valores, separados por linha.",
+            )
+            aproximacoes = []
+            for i in range(n):
+                xi_str = input(f"Insira a aproximação para x{i + 1}: ").strip()
+                xi = ler_valor_matematico(xi_str)
+                aproximacoes.append(float(xi))
+            matriz_xi = criar_matriz(aproximacoes)
+
+            tol = float(input("\nInsira a tolerância absoluta: "))
+            n_max = int(input("Insira o número máximo de iterações: "))
+            if n_max <= 0:
+                raise ValueError("Erro: Número inválido\n")
+
+            rodando = input("\nDeseja continuar? (s/n) ").strip().lower() == "s"
+        except SympifyError as e:
+            print(f"Erro de sintaxe: {e}")
+        except ValueError as e:
+            print(e)
+        except Exception as e:
+            print(f"Erro inesperado: {e}")
+
 
 main()
